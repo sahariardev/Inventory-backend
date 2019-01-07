3b54170f69804ac166431a3d6e2925c339a12ea5
sahariardev <sahariaralamk@gmail.com>
2019-01-07 20:24:57 +0600

    project initailized

---
+++ src/main/java/com/sahariar/InventoryKnitGarden/InventoryKnitGardenApplication.java
@@ -0,0 +1,14 @@
+package com.sahariar.InventoryKnitGarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryKnitGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryKnitGardenApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/sahariar/InventoryKnitGarden/InventoryKnitGardenApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sahariar.InventoryKnitGarden;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InventoryKnitGardenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
