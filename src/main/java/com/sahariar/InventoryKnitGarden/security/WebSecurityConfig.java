bae50d8bf545e0ea1f73f3c4f3b6164337a553e8
sahariardev <sahariaralamk@gmail.com>
2019-01-12 14:27:09 +0600

    jwtprovider added

--- src/main/java/com/sahariar/InventoryKnitGarden/security/WebSecurityConfig.java
+++ src/main/java/com/sahariar/InventoryKnitGarden/security/WebSecurityConfig.java
@@ -1,10 +1,18 @@
 package com.sahariar.InventoryKnitGarden.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.sahariar.InventoryKnitGarden.services.UserDetailsServiceImpl;
 
 
 @Configuration
@@ -17,5 +25,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	AuthEntryPoint authEntryPoint;
 	
+	@Autowired
+	UserDetailsServiceImpl userDetailService;
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	@Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(userDetailService)
+                .passwordEncoder(passwordEncoder());
+    }
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		http.cors().and().csrf().disable().authorizeRequests().antMatchers("/api/auth/**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.exceptionHandling().authenticationEntryPoint(authEntryPoint)
+		.and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
 
 }
