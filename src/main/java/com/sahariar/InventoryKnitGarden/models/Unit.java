5db8f6953591b35c9491d88a74b86f621d627faa
sahariardev <sahariaralamk@gmail.com>
2019-01-22 21:21:51 +0600

    Unit added

---
+++ src/main/java/com/sahariar/InventoryKnitGarden/models/Unit.java
@@ -0,0 +1,47 @@
+package com.sahariar.InventoryKnitGarden.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Unit {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String symbol;
+	private String description;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public String toString() {
+		return "Unit [id=" + id + ", name=" + name + ", symbol=" + symbol + ", description=" + description + "]";
+	}
+	
+    
+}
---
+++ src/main/java/com/sahariar/InventoryKnitGarden/repositories/UnitRepository.java
@@ -0,0 +1,9 @@
+package com.sahariar.InventoryKnitGarden.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sahariar.InventoryKnitGarden.models.Unit;
+
+public interface UnitRepository extends JpaRepository<Unit,Long> {
+
+}
