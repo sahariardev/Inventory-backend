946148f3866617a89ea7c721dd4b92e906d15f68
sahariardev <sahariaralamk@gmail.com>
2019-01-23 22:05:00 +0600

    Unit added

--- src/main/java/com/sahariar/InventoryKnitGarden/models/Unit.java
+++ src/main/java/com/sahariar/InventoryKnitGarden/models/Unit.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Unit {
+public class Unit extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
