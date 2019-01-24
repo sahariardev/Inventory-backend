e2192d66a55ec98de88a7163b34383b7811c1001
sahariardev <sahariaralamk@gmail.com>
2019-01-24 21:10:02 +0600

    UnitController CrossOrigin added

--- src/main/java/com/sahariar/InventoryKnitGarden/controllers/UnitController.java
+++ src/main/java/com/sahariar/InventoryKnitGarden/controllers/UnitController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.sahariar.InventoryKnitGarden.models.Unit;
 import com.sahariar.InventoryKnitGarden.requests.UnitRequest;
 import com.sahariar.InventoryKnitGarden.services.UnitService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/units")
 public class UnitController {
