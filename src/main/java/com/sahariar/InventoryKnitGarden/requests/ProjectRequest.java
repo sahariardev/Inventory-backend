0082bd2902aeb652a8db6a12d90a3e12a58f2149
sahariardev <sahariaralamk@gmail.com>
2019-01-26 14:18:58 +0600

    Project and Style added

---
+++ src/main/java/com/sahariar/InventoryKnitGarden/requests/ProjectRequest.java
@@ -0,0 +1,29 @@
+package com.sahariar.InventoryKnitGarden.requests;
+
+public class ProjectRequest {
+
+	private String name;
+	private String description;
+	private Long client_id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Long getClient_id() {
+		return client_id;
+	}
+	public void setClient_id(Long client_id) {
+		this.client_id = client_id;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/sahariar/InventoryKnitGarden/requests/StyleRequest.java
@@ -0,0 +1,30 @@
+package com.sahariar.InventoryKnitGarden.requests;
+
+public class StyleRequest {
+
+	
+	private String name;
+	private String description;
+	private Long project_id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Long getProject_id() {
+		return project_id;
+	}
+	public void setProject_id(Long project_id) {
+		this.project_id = project_id;
+	}
+	
+	
+	
+}
