f98ed1308372131b31c6489125a71a5a76df4b23
sahariardev <sahariaralamk@gmail.com>
2019-01-28 21:14:04 +0600

    Stage model and request added

---
+++ src/main/java/com/sahariar/InventoryKnitGarden/requests/StageRequest.java
@@ -0,0 +1,18 @@
+package com.sahariar.InventoryKnitGarden.requests;
+
+
+
+public class StageRequest {
+
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
